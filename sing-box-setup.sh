e77cd5666751c30e810551119f19946970f51702
TinrLin <134979953+TinrLin@users.noreply.github.com>
2023-06-26 12:00:02 +0800

    Update sing-box-setup.sh

--- sing-box-setup.sh
+++ sing-box-setup.sh
@@ -84,7 +84,7 @@ read -p "请选择Shadowsocks加密方式：
 1. 2022-blake3-aes-128-gcm
 2. 2022-blake3-aes-256-gcm
 3. 2022-blake3-chacha20-poly1305
-请输入对应的数字 (默认3): " encryption_choice
+请输入对应的数字 (默认1): " encryption_choice
 encryption_choice=${encryption_choice:-1}
 
 case $encryption_choice in
@@ -104,10 +104,10 @@ case $encryption_choice in
         ss_password=$(openssl rand -base64 32)
         ;;
     *)
-        echo "无效的选择，默认使用2022-blake3-chacha20-poly1305加密方式。"
-        ss_method="2022-blake3-chacha20-poly1305"
-        shadowtls_password=$(openssl rand -base64 32)
-        ss_password=$(openssl rand -base64 32)
+        echo "无效的选择，默认使用2022-blake3-aes-128-gcm加密方式。"
+        ss_method="2022-blake3-aes-128-gcm"
+        shadowtls_password=$(openssl rand -base64 16)
+        ss_password=$(openssl rand -base64 16)
         ;;
 esac
 
